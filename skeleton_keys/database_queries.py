8a2fe86ed090292854274ffd4a14f9d6df081c51
Sarah Walling-Bell <sarah.wallingbell@alleninstitute.org>
2026-07-07 12:14:49 -0700

    update manual rotation values

--- skeleton_keys/database_queries.py
+++ skeleton_keys/database_queries.py
@@ -330,8 +330,8 @@ def pia_wm_soma_from_database(specimen_id, imser_id):
         919968983: 305,
         920009568: 25,
         1133498455: 256,
-        857499169: 90,
-        1402910918: 325
+        857499169: 270,
+        1402910918: 35
     }
     if specimen_id in special_cases.keys():
         rot_deg = special_cases[specimen_id]
@@ -482,8 +482,8 @@ def layer_polygons_from_database(image_series_id, specimen_id=None, soma_drawing
         919968983: 305,
         920009568: 25,
         1133498455: 256,
-        857499169: 90,
-        1402910918: 325
+        857499169: 279,
+        1402910918: 35
     }
     if (not specimen_id is None) and (not soma_drawing is None) and (specimen_id in special_cases.keys()):
         rot_deg = special_cases[specimen_id]
